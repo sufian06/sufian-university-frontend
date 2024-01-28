63509a1e4067dcd762e76b252045456c46ef3bef
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-01-28 21:49:59 +0600

    create reusable form with input field used react-hook-form

---
+++ src/components/Forms/FormInput.tsx
@@ -0,0 +1,49 @@
+"use client";
+
+import { Input } from "antd";
+import { Controller, useFormContext } from "react-hook-form";
+
+interface IInput {
+  name: string;
+  type?: string;
+  size?: "large" | "small";
+  value?: string | string[] | undefined;
+  id?: string;
+  placeholder?: string;
+  validation?: object;
+  label?: string;
+}
+
+const FormInput = ({
+  name,
+  type,
+  size,
+  value,
+  id,
+  placeholder,
+  validation,
+  label,
+}: IInput) => {
+  const { control } = useFormContext();
+
+  return (
+    <>
+      {label ? label : null}
+      <Controller
+        control={control}
+        name={name}
+        render={({ field }) => (
+          <Input
+            type={type}
+            size={size}
+            placeholder={placeholder}
+            {...field}
+            value={value ? value : field.value}
+          />
+        )}
+      />
+    </>
+  );
+};
+
+export default FormInput;
