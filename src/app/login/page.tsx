0d89db237cfca9048cfd9c949a0a109247c09d93
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-01-28 22:22:46 +0600

    login page created used Form & FormInput

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,68 @@
+"use client";
+
+import loginImage from "@/assets/login-image.png";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
 const LoginPage = () => {
+  const onSubmit: SubmitHandler<FormValues> = (data) => {
+    try {
+      console.log(data);
+    } catch (err) {}
+  };
+
   return (
-    <div>
-      <h1>This is login page</h1>
-    </div>
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={8}>
+        <Image src={loginImage} alt="Login Image" width={500} />
+      </Col>
+
+      <Col sm={12} md={8} lg={6}>
+        <h1
+          style={{
+            margin: "15px 0",
+          }}
+        >
+          First login your account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div
+              style={{
+                margin: "15px 0",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
   );
 };
 
--- src/components/Forms/FormInput.tsx
+++ src/components/Forms/FormInput.tsx
@@ -32,15 +32,25 @@ const FormInput = ({
       <Controller
         control={control}
         name={name}
-        render={({ field }) => (
-          <Input
-            type={type}
-            size={size}
-            placeholder={placeholder}
-            {...field}
-            value={value ? value : field.value}
-          />
-        )}
+        render={({ field }) =>
+          type === "password" ? (
+            <Input.Password
+              type={type}
+              size={size}
+              placeholder={placeholder}
+              {...field}
+              value={value ? value : field.value}
+            />
+          ) : (
+            <Input
+              type={type}
+              size={size}
+              placeholder={placeholder}
+              {...field}
+              value={value ? value : field.value}
+            />
+          )
+        }
       />
     </>
   );
