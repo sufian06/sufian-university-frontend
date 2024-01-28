69ca16d8108d73e34cb4fae903fea618b0510f3f
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-01-28 09:51:01 +0600

    antDesign initialize and error, loading, not-found pages added in app folder

--- src/lib/Providers.tsx
+++ src/lib/Providers.tsx
@@ -1,10 +1,15 @@
 "use client";
 
 import { store } from "@/redux/store";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
 import { Provider } from "react-redux";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
-  return <Provider store={store}>{children}</Provider>;
+  return (
+    <Provider store={store}>
+      <AntdRegistry>{children}</AntdRegistry>
+    </Provider>
+  );
 };
 
 export default Providers;
