4fe24bc9d62b3a528b29627852deada248d4027e
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-02-01 16:57:09 +0600

    updated login page by login component and medata updated into login page, rootLayout metadata title updated

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,84 @@
+"use client";
+
+import loginImage from "@/assets/login-image.png";
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { useUserLoginMutation } from "@/redux/api/authApi";
+import { storeUserInfo } from "@/services/auth.service";
+import { Button, Col, Row } from "antd";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import { SubmitHandler } from "react-hook-form";
+
+type FormValues = {
+  id: string;
+  password: string;
+};
+
+const LoginPage = () => {
+  const [userLogin] = useUserLoginMutation();
+  const router = useRouter();
+
+  // console.log(isLoggedIn());
+
+  const onSubmit: SubmitHandler<FormValues> = async (data: any) => {
+    try {
+      const res = await userLogin({ ...data }).unwrap();
+      if (res?.accessToken) {
+        router.push("/profile");
+      }
+      storeUserInfo({ accessToken: res?.accessToken });
+      // console.log(res);
+    } catch (err: any) {
+      console.error(err.message);
+    }
+  };
+
+  return (
+    <Row
+      justify="center"
+      align="middle"
+      style={{
+        minHeight: "100vh",
+      }}
+    >
+      <Col sm={12} md={16} lg={8}>
+        <Image src={loginImage} alt="Login Image" width={500} />
+      </Col>
+
+      <Col sm={12} md={8} lg={6}>
+        <h1
+          style={{
+            margin: "15px 0",
+          }}
+        >
+          First login your account
+        </h1>
+        <div>
+          <Form submitHandler={onSubmit}>
+            <div>
+              <FormInput name="id" type="text" size="large" label="User Id" />
+            </div>
+            <div
+              style={{
+                margin: "15px 0",
+              }}
+            >
+              <FormInput
+                name="password"
+                type="password"
+                size="large"
+                label="Password"
+              />
+            </div>
+            <Button type="primary" htmlType="submit">
+              Login
+            </Button>
+          </Form>
+        </div>
+      </Col>
+    </Row>
+  );
+};
+
+export default LoginPage;
