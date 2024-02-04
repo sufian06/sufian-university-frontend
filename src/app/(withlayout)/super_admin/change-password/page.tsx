4d06f46ecbc1b78349376d3a7145d350b6a900ea
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-02-04 13:32:30 +0600

    change-password added under super_admin

--- src/app/(withlayout)/super_admin/change-password/page.tsx
+++ src/app/(withlayout)/super_admin/change-password/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
+import { Button } from "antd";
+
 const ResetPassPage = () => {
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
   return (
-    <div>
-      <h1>Reset password page</h1>
+    <div
+      style={{ margin: "100px 0", display: "flex", justifyContent: "center" }}
+    >
+      <Form submitHandler={onSubmit}>
+        <h3 style={{ marginBottom: "10px" }}>Reset Password</h3>
+        <div style={{ margin: "10px 0" }}>
+          <FormInput name="oldPassword" label="Old Password" type="password" />
+        </div>
+        <div style={{ margin: "10px 0" }}>
+          <FormInput name="newPassword" label="New Password" type="password" />
+        </div>
+        <Button type="primary" htmlType="submit" style={{ marginTop: "10px" }}>
+          Password Reset
+        </Button>
+      </Form>
     </div>
   );
 };
