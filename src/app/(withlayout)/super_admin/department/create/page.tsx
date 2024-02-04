8dcc91c1a27b2d19697df1d6aa51ad15156dbe4d
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-02-04 13:21:27 +0600

    createDepartment added under super_admin

--- src/app/(withlayout)/super_admin/department/create/page.tsx
+++ src/app/(withlayout)/super_admin/department/create/page.tsx
@@ -1,6 +1,21 @@
+"use client";
+
+import Form from "@/components/Forms/Form";
+import FormInput from "@/components/Forms/FormInput";
 import SUMBreadCrumb from "@/components/ui/SUMBreadCrumb";
+import { createDepartmentSchema } from "@/schemas/createDepartment";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { Button, Col, Row } from "antd";
 
 const CreateDepartmentPage = () => {
+  const onSubmit = async (data: any) => {
+    try {
+      console.log(data);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
   const base = "super_admin";
   return (
     <div>
@@ -10,7 +25,20 @@ const CreateDepartmentPage = () => {
           { label: "department", link: `/${base}/department` },
         ]}
       />
-      <h1>Create department page</h1>
+      <h1>Create Department</h1>
+      <Form
+        submitHandler={onSubmit}
+        resolver={yupResolver(createDepartmentSchema)}
+      >
+        <Row gutter={{ xs: 24, xl: 8, lg: 8, md: 24 }}>
+          <Col span={8} style={{ margin: "10px 0" }}>
+            <FormInput name="title" label="Title" />
+          </Col>
+        </Row>
+        <Button type="primary" htmlType="submit">
+          Add Department
+        </Button>
+      </Form>
     </div>
   );
 };
